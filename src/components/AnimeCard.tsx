35b12439e6f7a6ef4f8b5bf1e70453385593ff35
Kev <sulpeum@gmail.com>
2023-02-19 00:03:28 -0500

    added link functionality to images

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Genre, Aired } from "../interfaces/interfaceTop100Anime";
 import { Link } from "react-router-dom";
+import { Navigate, useNavigate, redirect } from "react-router-dom";
 
 type Props = {
   id: number;
@@ -29,48 +30,55 @@ const AnimeCard = ({
   removeWrittenByMALRewrite,
   truncateString,
 }: Props) => {
+  //Navigate to a singular anime's page since you can't have an img element inside a Link element
+  const navigate = useNavigate();
+  const move = (url: string) => {
+    return navigate(url);
+  };
+
   return (
-    <Link to={`/anime/${id}`}>
-      <div className="flex h-[400px] flex-col rounded-md bg-white shadow-md ring-1 ring-black/[.05] sm:h-[275px]  sm:max-w-xl sm:flex-row">
-        {/* Left side */}
+    <div className="flex h-[400px] flex-col rounded-md bg-white shadow-md ring-1 ring-black/[.05] sm:h-[275px]  sm:max-w-xl sm:flex-row">
+      {/* Left side */}
 
-        <img
-          className="h-full w-full rounded-t-md object-cover object-[top_center] sm:h-auto sm:w-48 sm:rounded-none sm:rounded-l-md"
-          src={imageURL}
-          alt={title}
-        ></img>
+      <img
+        className="h-full w-full cursor-pointer rounded-t-md object-cover object-[top_center] sm:h-auto sm:w-48 sm:rounded-none sm:rounded-l-md"
+        src={imageURL}
+        alt={title}
+        onClick={() => move(`/anime/${id}`)}
+      ></img>
 
-        {/* Right side */}
-        <div className="flex flex-col justify-between">
-          <div className="flex h-full flex-col justify-between gap-4 p-3">
-            {/* Title + Air Date */}
-            <div className="flex flex-col gap-1">
+      {/* Right side */}
+      <div className="flex flex-col justify-between">
+        <div className="flex h-full flex-col justify-between gap-4 p-3">
+          {/* Title + Air Date */}
+          <div className="flex flex-col gap-1">
+            <Link to={`/anime/${id}`}>
               <h2 className="text-xl font-semibold tracking-tighter text-[#393831]">
                 {title}
               </h2>
-              <p className="text-xs font-semibold">
-                {episodes} episodes aired on {removeExtraDate(aired.string)}
-              </p>
-            </div>
-            {/* Sypnosis Content */}
-            <p className="max-h-[10rem] max-w-full overflow-y-auto overscroll-contain  text-[0.7rem] leading-5  text-text">
-              {truncateString(removeWrittenByMALRewrite(synopsis), synopsisNum)}
+            </Link>
+            <p className="text-xs font-semibold">
+              {episodes} episodes aired on {removeExtraDate(aired.string)}
             </p>
           </div>
-          {/* Footer */}
-          <footer className="flex h-[3.5rem] max-h-[3.5rem] w-full flex-wrap items-center gap-1  rounded-br-md bg-gray pl-3">
-            {genres.map((item) => (
-              <div
-                className="flex max-h-[2rem] max-w-fit items-center justify-center rounded-2xl bg-[#3480EA] px-2 py-1 pb-1.5 text-[0.65rem] font-semibold text-white shadow-md"
-                key={item.mal_id}
-              >
-                {item.name}
-              </div>
-            ))}
-          </footer>
+          {/* Sypnosis Content */}
+          <p className="max-h-[10rem] max-w-full overflow-y-auto overscroll-contain  text-[0.7rem] leading-5  text-text">
+            {truncateString(removeWrittenByMALRewrite(synopsis), synopsisNum)}
+          </p>
         </div>
+        {/* Footer */}
+        <footer className="flex h-[3.5rem] max-h-[3.5rem] w-full flex-wrap items-center gap-1  rounded-br-md bg-gray pl-3">
+          {genres.map((item) => (
+            <div
+              className="flex max-h-[2rem] max-w-fit items-center justify-center rounded-2xl bg-[#3480EA] px-2 py-1 pb-1.5 text-[0.65rem] font-semibold text-white shadow-md"
+              key={item.mal_id}
+            >
+              {item.name}
+            </div>
+          ))}
+        </footer>
       </div>
-    </Link>
+    </div>
   );
 };
 
